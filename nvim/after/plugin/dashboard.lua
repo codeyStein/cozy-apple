41d22bab7b9b96cd7c7c3505a744c9970f6fd2b8
codeyStein <san.26.2010@icloud.com>
2022-12-29 18:06:15 -0600

    Fix Dashboard

--- nvim/after/plugin/dashboard.lua
+++ nvim/after/plugin/dashboard.lua
@@ -2,61 +2,46 @@
 local status, db = pcall(require, "dashboard")
 local home = os.getenv("HOME")
 
-db.default_banner = {
-	"",
-	"",
-	" ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗",
-	" ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║",
-	" ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║",
-	" ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║",
-	" ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║",
-	" ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝",
-	"",
-	" [ TIP: To exit Neovim, just power off your computer. ] ",
-	"",
+db.custom_header = {
+    "",
+    "                         _.'.__",
+    "                      _.'      .",
+    "':'.               .''   __ __  .",
+    "  '.:._          ./  _ ''     '-'.__",
+    ".'''-: '''-._    | .                '-'._",
+    " '.     .    '._.'                       '",
+    "    '.   '-.___ .        .'          .  :o'.",
+    "      |   .----  .      .           .'     (",
+    "       '|  ----. '   ,.._                _-'",
+    "        .' .---  |.''  .-:;.. _____.----'",
+    "        |   .-''''    |      '",
+    "      .'  _'         .'    _'",
+    "     |_.-'            '-.'",
+    "",
+    '       "七転び八起き"     ',
+    "",
 }
--- linux
---db.preview_command = 'ueberzug'
---
---db.preview_file_path = home .. '/.config/nvim/static/neovim.cat'
+
 db.preview_file_height = 11
 db.preview_file_width = 70
 db.custom_center = {
 	{
-		icon = "  ",
-		desc = "Recent sessions                         ",
-		shortcut = "SPC s l",
-		action = "SessionLoad",
-	},
-	{
 		icon = "  ",
-		desc = "Find recent files                       ",
+		desc = "Open Tree                               ",
 		action = "Telescope oldfiles",
-		shortcut = "SPC pf",
-	},
-	{
-		icon = "  ",
-		desc = "Find files                              ",
-		action = "Telescope find_files find_command=rg,--hidden,--files",
-		shortcut = "SPC pf",
+		shortcut = "<LEADER> e",
 	},
 	{
 		icon = "  ",
 		desc = "File browser                            ",
 		action = "Telescope file_browser",
-		shortcut = "SPC pf",
+		shortcut = "<LEADER> pf",
 	},
 	{
 		icon = "  ",
 		desc = "Find word                               ",
 		action = "Telescope live_grep",
-		shortcut = "CRL ps",
-	},
-	{
-		icon = "  ",
-		desc = "Load new theme                          ",
-		action = "Telescope colorscheme",
-		shortcut = "SPC h t",
+		shortcut = "<LEADER> ps",
 	},
 }
 db.custom_footer = { "", ":q to exit vim" }
